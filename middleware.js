0f78861d6a0a34f71f4caaadb600200a27fc1d11
Nathan-Scotty <qualitechoisie13@gmail.com>
2026-07-21 14:08:01 -0400

    addding middleware

---
+++ middleware.js
@@ -0,0 +1,15 @@
+import { NextResponse } from 'next/server';
+
+export function middleware(req) {
+  const token = req.cookies.get('authToken')?.value;
+
+  if (!token) {
+    return NextResponse.redirect(new URL('/signin', req.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ['/admin'],
+};
